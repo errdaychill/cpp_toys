87e8e835df2a2404d31064dca790e8a8204939df
kaimph94 <60500293+errdaychill@users.noreply.github.com>
2020-07-14 22:29:45 +0900

    header file

---
+++ Matrix.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <random>
+#include <cmath>
+
+class Matrix
+{
+private:
+        int row_;
+        int col_;
+        double **mat;
+public:
+        
+};
