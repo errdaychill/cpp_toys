93d939fecfd0386ad6f1fe09fec3b44f2e9edd65
kaimph94 <60500293+errdaychill@users.noreply.github.com>
2020-07-14 22:29:21 +0900

    main file

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Something
+{
+    private:
+        int m_value = 1;
+    
+}
+
+void doSomething(A& a)
+{
+    cout << a. 
+}
+
+int main()
+{
+    Something s1;
+    Something s2;
+    
+    
+    return 0;
+}
